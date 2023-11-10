7cf2da79aaf40ed647b59448c5952ee0827d0232
Acoidan Martín Conrado <alu0101623627@ull.edu.es>
2023-11-10 21:40:11 +0000

    minor changes

--- arrays/P87920/P87920.cc
+++ arrays/P87920/P87920.cc
@@ -10,19 +10,6 @@
   * @bug No known bugs
   * @see https://jutge.org/problems/P87920_en
 */
-/**
- * Universidad de La Laguna
- * Escuela Superior de Ingeniería y Tecnología
- * Grado en Ingeniería Informática
- * Informática Básica 2022-2023
- *
- * @file  P87920-EqualToSumOfRest_2.cc
- * @author Alvaro Perez Ramos alu0101574042@ull.edu.es
- * @date Nov 21 2022
- * @brief Write a program that, given a sequence of integer numbers, tells if
- * there is any number equal to the sum of the rest.
- * @see https://jutge.org/problems/P87920
- */
 #include <iostream>
 #include <vector>
 using namespace std;
